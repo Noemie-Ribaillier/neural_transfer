de8f85c675c574ad26f18abddc13d657d07e2257
Noemie Ribaillier <noemie.ribaillier@gmail.com>
2025-02-25 14:11:11 +0100

    update the files to make it clearer

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# neural_transfer
+# Neural style transfer
+This project aims at learning about neural style transfer. Neural Style Learning aims at generating an image (G) from a content image (C) and a style image (S). It creates a new image by applying the style of an the image S to the content of the image C.
+
+
+## Project description
+In this project, we will combine the Louvre museum in Paris (content image C) with the impressionist style of Claude Monet (style image S) to generate a new image (Louvre picture with Monet style).
+
+
+## Model used
+We use a previously trained CNN and we build on top of that, this is called transfer learning (training a network on a  different task and applying it to a new task).
+For this project, we use the VGG-19, which is a 19-layers version of the VGG network.
+This model has already been trained on the very large ImageNet database, and has learned to recognize a variety of low level features (at the shallower layers) and high level features (at the deeper layers).
+
+NST aims at building a model in which the optimization algorithm updates the pixel values rather than the neural network's parameters (usually a model updates the NN parameters). 
+
+### Layers
+When performing NST, the goal is for the content in generated image G to match the content of image C. To do so, we need the importance and use of the different layers:
+* The shallower layers of a CNN tend to detect lower-level features such as edges and simple textures.
+* The deeper layers tend to detect higher-level features such as more complex textures and object classes. 
+We want the generated image G to have similar content as the input image C. In practice, we will usually get the most visually pleasing results if we choose a layer from somewhere in the middle of the network (neither too shallow nor too deep so that the network detects both higher-level and lower-level features).
+
+So if we want the generated image to softly follow the style image, we should choose larger weights for deeper layers and smaller weights for the first layers. On contrary, if we want the generated image to strongly follow the style image, we should choose smaller weights for deeper layers and larger weights for the first layers.
+
+In our case we choose these layers block1_conv1, block2_conv1, block3_conv1, block4_conv1, block5_conv1 because:
+* we are only interested in convolutional layers since those are the ones that learn and extract features from an image (so we don;t take the maxpool layers)
+* these layers capture low-level texture and style information that is crucial for defining the artistic style of the image. So using these layers reduces the risk of mixing content information into the style loss
+* reducing the number of layers involved in style loss calculation helps save computational resources
+
+## Gram matrix
+The Gram matrix is a way to grasp a style of an image and the measure of correlation between features after each layer. 
+The style matrix is also called a "Gram matrix". In linear algebra, the Gram matrix G of a set of vectors v_1,... ,v_n is the matrix of dot products, G_{ij} = v_{i}^T v_{j} = np.dot(v_{i}, v_{j}). So G_{ij} compares how similar v_i is to v_j (if they are highly similar, you would expect them to have a large dot product, and thus for G_{ij} to be large).
+We want to minimize the distance between the Gram matrix of the "style" image S and the Gram matrix of the "generated" image G. 
+
+
+## Script explanation, step by step
+1. Load the content image 
+2. Load the style image
+3. Randomly initialize the image to be generated 
+4. Load the VGG19 model
+5. Build and compute the content loss
+It represents the distance in content between the content image and the content of the generated image.
+6. Build and compute the style loss
+It represents the distance in style between the style image and the style of the generated image.
+7. Compute the total cost (using the content cost and style cost)
+8. Define the optimizer (and learning rate) and train the model
+
+
+## To continue
+Several steps can be done to go further with this project:
+* run the optimization algorithm longer (more epochs, for example 20000)
+* use a smaller learning rate (for example 0.001)
+* select different layers to represent the style (redefine STYLE_LAYERS)
+* alter the relative weight of content versus style (with changing alpha and beta values)
---
+++ script.py
@@ -0,0 +1,435 @@
+####################################################################################################
+#####                                                                                          #####
+#####                        DEEP LEARNING & ART: NEURAL STYLE TRANSFER                        #####
+#####                                  Created on: 2025-02-20                                  #####
+#####                                  Updated on: 2025-02-25                                  #####
+#####                                                                                          #####
+####################################################################################################
+
+####################################################################################################
+#####                                         PACKAGES                                         #####
+####################################################################################################
+
+# Clear the global environment
+globals().clear()
+
+# Load the libraries
+import sys
+import scipy.io
+import scipy.misc
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import pprint
+
+# Set up the right directory
+import os
+os.chdir('C:/Users/Admin/Documents/Python Projects/neural_transfer')
+
+
+####################################################################################################
+#####                                     TRANSFER LEARNING                                    #####
+####################################################################################################
+
+# Set up the seed to have reproducible results
+tf.random.set_seed(272)
+
+# Size of the image
+img_size = 400
+
+# Load the VGG19 model using the weights from the model pre-trained on ImageNet, using the following parameters:
+# include_top: to include (or not) the 3 fully-connected layers at the top of the network (last layers)
+# here we don't do classification problem so we don't need the last layers, we just need the layers which extract information/features
+# input_shape: to be specified since include_top is False (the 3 comes from RGB colors)
+vgg = tf.keras.applications.VGG19(include_top=False,
+                                  input_shape=(img_size, img_size, 3),
+                                  weights='imagenet')
+
+# To do transfer learning we set up the trainable to False (freezing the weights of the VGG model)
+vgg.trainable = False
+
+# Print the structure or details of the VGG model, it makes it easier to understand and inspect the model
+# We get the model's architecture (including the layer names, types, parameters, outputs, etc.) in a table format
+vgg.summary()
+
+
+####################################################################################################
+#####                                       CONTENT COST                                       #####
+####################################################################################################
+
+# Create the function to compute the content cost
+def compute_content_cost(content_output, generated_output):
+    """
+    Computes the content cost
+    
+    Arguments:
+    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+    
+    Returns: 
+    J_content -- content cost (scalar)
+    """
+    a_C = content_output[-1]
+    a_G = generated_output[-1]
+        
+    # Retrieve dimensions from a_G
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Unroll (reshape) a_C and a_G (from (1,n_H,n_W,n_C) to (1, n_H * n_W, n_C))
+    a_C_unrolled = tf.reshape(a_C, shape=[1, n_H * n_W, n_C]) 
+    a_G_unrolled = tf.reshape(a_G, shape=[1, n_H * n_W, n_C]) 
+    
+    # Compute the cost 
+    J_content = (1/(4*n_H*n_W*n_C))*tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled,a_G_unrolled)))
+
+    return J_content
+
+
+####################################################################################################
+#####                          GRAM MATRIX (ALSO CALLED STYLE AMTRIX)                          #####
+####################################################################################################
+
+# Create a function to compute the Gram matrix
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+    
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """
+
+    # Formula of  G_A = AA^T
+    GA = tf.linalg.matmul(A,tf.transpose(A))
+
+    return GA
+
+
+####################################################################################################
+#####                                        STYLE COST                                        #####
+####################################################################################################
+
+# We want to minimize the distance between the Gram matrix of the "style" image S and 
+# the Gram matrix of the "generated" image G
+
+# Create the function to compute the style cost for a single layer
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image S 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image G
+    
+    Returns: 
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    # Retrieve dimensions from the hidden layer a_G
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Unroll (reshape) the tensors from (1, n_H, n_W, n_C) to (n_C, n_H * n_W)
+    a_S = tf.transpose(tf.reshape(a_S,shape=[n_H * n_W,n_C]))
+    a_G = tf.transpose(tf.reshape(a_G,shape=[n_H * n_W,n_C]))
+
+    # Computing Gram_matrices for both images S and G
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the loss (style cost)
+    J_style_layer = (1/(4*((n_H*n_W)**2)*(n_C**2)))*tf.reduce_sum(tf.square(tf.subtract(GS,GG)))
+
+    return J_style_layer
+
+
+####################################################################################################
+#####                                       STYLE WEIGHTS                                      #####
+####################################################################################################
+
+# We get better results if we "merge" style costs from several layers (proportional sum of the style cost of each layer)
+# (each layer gets a weight that reflects how much this layer contributes to the style)
+
+# List the layer names
+for layer in vgg.layers:
+    print(layer.name)
+
+# Get a look at the output of a layer, for example 'block5_conv4'
+vgg.get_layer('block5_conv4').output
+
+# Choose layers to represent the style of the image and assign style costs 
+# (by default we assign equal weight to each layer, and weights add up to 1)
+STYLE_LAYERS = [
+    ('block1_conv1', 0.2),
+    ('block2_conv1', 0.2),
+    ('block3_conv1', 0.2),
+    ('block4_conv1', 0.2),
+    ('block5_conv1', 0.2)]
+
+# Create the function to compute the overall style cost (from several chosen layers)
+def compute_style_cost(style_image_output, generated_image_output, STYLE_LAYERS=STYLE_LAYERS):
+    """
+    Computes the overall style cost from several chosen layers
+    
+    Arguments:
+    style_image_output -- our tensorflow model
+    generated_image_output --
+    STYLE_LAYERS -- A python list containing:
+                        - the names of the layers we would like to extract style from
+                        - a coefficient for each of them
+    
+    Returns: 
+    J_style -- tensor representing a scalar value
+    """
+    
+    # Initialize the overall style cost
+    J_style = 0
+
+    # Set a_S to be the hidden layer activation from the layer we have selected.
+    # The last element of the array contains the content layer image, which must not be used.
+    a_S = style_image_output[:-1]
+
+    # Set a_G to be the output of the choosen hidden layers.
+    # The last element of the list contains the content layer image which must not be used.
+    a_G = generated_image_output[:-1]
+
+    # For each layer:
+    for i, weight in zip(range(len(a_S)), STYLE_LAYERS):  
+
+        # Get the style of the style image "S" from the current layer
+        a_S_i = a_S[i]
+
+        # Get the style of the generated image "G" from the current layer
+        a_G_i = a_G[i]
+
+        # Compute style_cost for the current layer
+        J_style_layer = compute_layer_style_cost(a_S_i, a_G_i)
+
+        # Add weight * J_style_layer of this layer to overall style cost (add the weighted style cost to the overall style cost)
+        J_style += weight[1] * J_style_layer
+
+    # Return the overall style cost 
+    return J_style
+
+
+####################################################################################################
+#####                           DEFINING THE TOTAL COST TO OPTIMIZE                            #####
+####################################################################################################
+
+# Create the total cost function (that minimizes both the style and the content cost)
+# Total cost is linear combination of the content cost and the style cost
+# alpha and beta are hyperparameters that control the relative weighting between content and style
+
+# To convert a Python function into a TF graph operation (which can lead to faster and more efficient execution compared to eager execution)
+# It will significantly improve the performance of our code by enabling TF to optimize and parallelize the computations
+@tf.function()
+def total_cost(J_content, J_style, alpha = 10, beta = 40):
+    """
+    Computes the total cost function
+    
+    Arguments:
+    J_content -- content cost
+    J_style -- style cost
+    alpha -- hyperparameter weighting the importance of the content cost
+    beta -- hyperparameter weighting the importance of the style cost
+    
+    Returns:
+    J -- total cost as defined by the formula above.
+    """
+
+    J = alpha*J_content+beta*J_style
+    
+    return J
+
+
+####################################################################################################
+#####                                  LOAD THE CONTENT IMAGE                                  #####
+####################################################################################################
+
+# Load and reshape the "content" image C
+content_image = np.array(Image.open("images/persian_cat.jpg").resize((img_size, img_size)))
+content_image = tf.constant(np.reshape(content_image, ((1,) + content_image.shape)))
+
+# Show the content image C and its shape
+print(content_image.shape)
+imshow(content_image[0])
+plt.axis('off')
+plt.show()
+
+
+####################################################################################################
+#####                                   LOAD THE STYLE IMAGE                                   #####
+####################################################################################################
+
+# Load and reshape the "style" image S
+style_image =  np.array(Image.open("images/monet.jpg").resize((img_size, img_size)))
+style_image = tf.constant(np.reshape(style_image, ((1,) + style_image.shape)))
+
+# Show the style image S and its shape
+print(style_image.shape)
+imshow(style_image[0])
+plt.axis('off')
+plt.show()
+
+
+####################################################################################################
+#####                       RANDOMLY INITIALIZE THE IMAGE TO BE GENERATED                      #####
+####################################################################################################
+
+# Set up generated image to content image (Variable to be mutuable and float32 to normalize)
+generated_image = tf.Variable(tf.image.convert_image_dtype(content_image, tf.float32))
+
+# Create uniform noise with shape of the generated image
+noise = tf.random.uniform(tf.shape(generated_image), -0.25, 0.25)
+
+# Add this noise to the generated image, to create a noisy image of the content image (being the generated image)
+generated_image = tf.add(generated_image, noise)
+
+# Set up the values between 0 and 1
+generated_image = tf.clip_by_value(generated_image, clip_value_min=0.0, clip_value_max=1.0)
+
+print(generated_image.shape)
+imshow(generated_image.numpy()[0])
+plt.show()
+
+
+####################################################################################################
+#####                               LOAD PRE-TRAINED VGG19 MODEL                               #####
+####################################################################################################
+
+# Define a function which loads the model and returns a list of the outputs for the middle layers
+def get_layer_outputs(vgg, layer_names):
+
+    """ Creates a vgg model that returns a list of intermediate output values."""
+    
+    outputs = [vgg.get_layer(layer[0]).output for layer in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+# Define the content layer and build the model
+content_layer = [('block5_conv4', 1)]
+vgg_model_outputs = get_layer_outputs(vgg, STYLE_LAYERS + content_layer)
+
+# Save the outputs for the content and style layers in separate variables.
+content_target = vgg_model_outputs(content_image)  # Content encoder
+style_targets = vgg_model_outputs(style_image)     # Style encoder
+
+ 
+####################################################################################################
+#####                                   COMPUTE CONTENT COST                                   #####
+####################################################################################################
+
+# To compute the content cost, we encode (a_C) our content image using the appropriate hidden layer activations. 
+
+# Assign the content image to be the input of the VGG model.  
+# Set a_C to be the hidden layer activation from the layer we have selected
+preprocessed_content =  tf.Variable(tf.image.convert_image_dtype(content_image, tf.float32))
+a_C = vgg_model_outputs(preprocessed_content)
+
+
+####################################################################################################
+#####                                    COMPUTE STYLE COST                                    #####
+####################################################################################################
+
+# Compute the style image encoding
+# a_S sets to be the tensor giving the hidden layer activation for `STYLE_LAYERS` using our style image.
+
+# Assign the input of the model to be the "style" image 
+preprocessed_style =  tf.Variable(tf.image.convert_image_dtype(style_image, tf.float32))
+a_S = vgg_model_outputs(preprocessed_style)
+
+# To convert a given tensor into a PIL image
+def tensor_to_image(tensor):
+    """
+    Converts the given tensor into a PIL image
+    
+    Arguments:
+    tensor -- Tensor
+    
+    Returns:
+    Image: A PIL image
+    """
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    # If it has 4 dimensions (because added dimension for the batch)
+    if np.ndim(tensor) > 3:
+        # We check that indeed the 1st dimension (batch dimension) is equal to 1 (batch of only 1 image). Otherwise it will raise an error
+        assert tensor.shape[0] == 1
+        # In that case, we get the 1st image, meaning we remove the batch dimension
+        tensor = tensor[0]
+    
+    # Converts the input NumPy array (tensor) into a PIL Image object
+    return Image.fromarray(tensor)
+
+
+####################################################################################################
+#####                                      TRAIN THE MODEL                                     #####
+####################################################################################################
+
+# Use Adam optimizer to minimize the total cost with a 0.01 learning rate
+# If we increase the learning rate we can speed up the style transfer, but often at the cost of quality.
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+# We implement the train_step() function for transfer learning
+@tf.function()
+def train_step(generated_image):
+
+    # Perform a step of gradient-based optimization where generated_image is updated to minimize the loss function J
+    # Records all operations performed on tensors inside the with block so that the gradients can be computed later
+    with tf.GradientTape() as tape:
+
+        # Compute a_G as the vgg_model_outputs for the current generated image
+        a_G = vgg_model_outputs(generated_image)
+        
+        # Compute the style cost
+        J_style = compute_style_cost(a_S, a_G, STYLE_LAYERS=STYLE_LAYERS)
+        
+        # Compute the content cost
+        J_content = compute_content_cost(a_C, a_G)
+
+        # Compute the total cost
+        J = total_cost(J_content, J_style, alpha = 10, beta = 40)
+
+    # Computes the gradient of the loss function J with respect to the variable generated_image
+    grad = tape.gradient(J, generated_image)
+
+    # Applies the computed gradient to generated_image, updating it in the direction that reduces the loss function J
+    optimizer.apply_gradients([(grad, generated_image)])
+    
+    # generated_image.assign(clip_0_1(generated_image))
+    # Set up balues between 0 and 1
+    generated_image.assign(tf.clip_by_value(generated_image, clip_value_min=0.0, clip_value_max=1.0))
+    
+    return J
+
+# Use tf.Variable to make the generated_image mutable (so that it can be modified during the training or optimization process)
+# The variable will track its gradients, allowing TensorFlow to adjust its values based on some loss function or optimization algorithm
+generated_image = tf.Variable(generated_image)
+
+# Choose 2501 epochs (we run 2501 times the model)
+epochs = 501
+
+# Show the generated image every 250 (meaning the script stops to run until we don't close the temp generated image)
+# Save the images generated every 250 epochs to keep track of the generated image
+for i in range(epochs):
+    # print(i)
+    train_step(generated_image)
+    if i % 250 == 0:
+        print(f"Epoch {i} ")
+    if i % 250 == 0:
+        image = tensor_to_image(generated_image)
+        imshow(image)
+        image.save(f"output/cat_image_{i}.jpg")
+        plt.show() 
+
+# Show the 3 images at the same time: the content image, the style image and the generated image (content mixed with style image)
+fig = plt.figure(figsize=(16, 4))
+ax = fig.add_subplot(1, 3, 1)
+imshow(content_image[0])
+ax.title.set_text('Content image')
+ax = fig.add_subplot(1, 3, 2)
+imshow(style_image[0])
+ax.title.set_text('Style image')
+ax = fig.add_subplot(1, 3, 3)
+imshow(generated_image[0])
+ax.title.set_text('Generated image')
+plt.show()
